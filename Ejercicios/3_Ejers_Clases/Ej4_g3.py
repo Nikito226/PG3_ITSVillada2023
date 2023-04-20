d9a2552fe443ab9be93e5c7a83041fe8413331da
Nico <nicolasbetg@gmail.com>
2023-04-20 19:42:26 -0300

    Ej4_g3

---
+++ Ejercicios/3_Ejers_Clases/Ej4_g3.py
@@ -0,0 +1,18 @@
+def division():
+    while True:
+        try:
+            n1 = int(input("Ingresa el primer numero:\n>>> "))
+            n2 = int(input("Ingresa el segundo numero:\n>>> "))
+
+            print(n1/n2)
+
+        except ZeroDivisionError:
+            print("No se puede dividir por cero")
+        except ValueError:
+            print("Ingresa un numero")
+
+        finally:
+            print("Proceso Ejecutado")
+
+
+division()
